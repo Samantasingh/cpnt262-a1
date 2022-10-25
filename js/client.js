8934e4a1b9ad7a501894b8333dc4823e67d5a689
Samanta S <samantalsingh@gmail.com>
2022-10-24 19:50:53 -0600

    added funtions

---
+++ js/client.js
@@ -0,0 +1,18 @@
+
+  let button = document.querySelector('button');
+  button.addEventListener("click", changeTXT);
+  let p1 = document.querySelector('p')
+  let text = 'Your assignment has been successfully submitted. Check your mail for the automated comfirmation email.';
+
+  function changeTXT() {
+    p.innerHTML = text;
+  }
+
+  let number = 7.836;
+  let rounded = number. toFixed();
+  document.getElementById ("rounded").innerHTML = rounded;
+
+  function changeTXT() {
+    p1.innerHTML = text;
+  
+  }
